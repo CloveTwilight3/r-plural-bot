77d0580ea7e3979db28692b0a002149cca5860ab
CloveTwilight3 <clovetwilight3@outlook.com>
2025-08-08 20:58:34 +0100

    Add in dummy roles

---
+++ src/roles/config.ts
@@ -0,0 +1,48 @@
+export const ROLES = {
+    // Formatted 'emoji id': 'role id' // Role Name
+  type: {
+    '321': '123', // Endogenic
+    '123': '123', // Traumagenic
+    '213': '123' // Unsure
+  },
+  pronouns: {
+    '111': '123', // She/Her
+    '1': '123', // He/Him
+    '11': '123', // He/She
+    '1111': '123', // She/They
+    '11111': '123', // He/They
+    '111111': '123', // They/She
+    '1111111': '123', // They/He
+    '11111111': '123', // They/Them
+    '111111111': '123', // Fae/Faer
+    '1111111111': '123', // She/Fae
+    '11111111111': '123', // Xe/Xem
+    '123': '123', // Ze/Zir
+    '2': '123', // She/It
+    '3': '123', // He/It
+    '4': '123', // They/It
+    '5': '123', // It/Its
+    '6': '123', // She/Any
+    '7': '123', // He/Any
+    '8': '132', // She/They/He
+    '9': '123', // He/They/She
+    '10': '132', // They/She/Her
+    '12': '123', // They/He/She
+    '13': '123' // Ask
+  },
+  gender:{
+    '1': '132', // Cisgender
+    '123': '123', // Transfeminine
+    '321': '123', // Transmasculine
+    '312': '123', // Genderfluid
+    '213': '123' // Non-Binary
+  },
+  touch: {
+    '1': '123', // No Touching
+    '2': '123', // Ask Before Touching
+    '3': '123' // Touch Okay
+  },
+  other: {
+    '111': '123' // Gamer
+  }
+};
